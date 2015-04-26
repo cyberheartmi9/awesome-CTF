700e5991dd6b2707bd90e49af9b3f166a57a12cc
Amanpreet Singh <apsdehal@gmail.com>
2015-04-27 01:09:51 +0530

    Add all the styles and etc

---
+++ assets/awesome-ctf.js
@@ -0,0 +1,26 @@
+var getFileData = function(url) {
+  $.ajax({
+    url: url,
+    headers: {
+      "Accept": "application/vnd.github.v3.raw"
+    }
+  }).done(function(data) {
+    var converter = new Showdown.converter();
+    var html = converter.makeHtml(data);
+    $("#content").html(html)
+    var thanks = '<div id="thanks">Styles for the website were quickly taken from <a href="http://awesome-go.com/" title="Awesome Go">awesome-go</a></div>';
+    $("#content").append(thanks);
+  })
+};
+
+$(document).ready(function(){
+  $.getJSON("https://api.github.com/repos/apsdehal/ctf-tools/git/trees/HEAD").
+    done(function(data){
+      for (var i = data.tree.length - 1; i >= 0; i--) {
+        if(data.tree[i].path == "README.md") {
+          getFileData(data.tree[i].url);
+          break;
+        }
+      };
+    });
+});
